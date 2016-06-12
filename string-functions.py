a76fbe1da882d9a713fd51dca28f9535e046b901
MohamedFawzy <mfawzy22@gmail.com>
2016-06-13 01:04:55 +0200

    add string functions example in python

---
+++ string-functions.py
@@ -0,0 +1,25 @@
+__author__ = 'mohamed fawzy'
+
+s = 'Monty Python'
+print s[0:4] # print from index zero to 4 but index four not included so it will print index from 0 to 3
+
+print s[3:] # print from index 3 to the end of string
+
+print s[:5] # print from index 5 to the begining
+
+fruit = 'apple'
+print 'n' in fruit # return boolean
+print 'a' in fruit # return boolean
+
+# string contact
+a='hello'
+b= a + 'world'
+print b
+
+c = a + ' ' + 'There'
+print c
+
+name = 'Mohamed FAWZY'
+print name.lower() # to lower case
+print 'HEY'.upper() + ' '+ name.lower()
+
