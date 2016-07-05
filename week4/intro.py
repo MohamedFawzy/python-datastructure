68a24680836f50595f28f18e03c4b3f83c4c36b3
MohamedFawzy <mfawzy22@gmail.com>
2016-07-05 04:45:51 +0200

    init week4

---
+++ week4/intro.py
@@ -0,0 +1,8 @@
+friends = ['mohamed', 'ahmed', 'fawzy']
+
+for friend in friends:
+    print 'Happy New Year', friend
+
+for i in range(len(friends)):
+    friend = friends[i]
+    print 'Happy New Year-->', friend
---
+++ week4/methods.py
@@ -0,0 +1,19 @@
+x = list()
+#print type(x)
+#print dir(x)
+x.append('book')
+x.append(99)
+print x
+
+# check if there's something in list
+print 'book' in x
+print 'lol' not in x
+friends = ['mohamed', 'ahmed', 'amr', 'joe']
+print friends
+friends.sort()
+print friends
+nums = [1,2,4,6,6,23]
+print len(nums)
+print min(nums)
+print max(nums)
+print sum(nums)
